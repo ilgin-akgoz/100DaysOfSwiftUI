39d53fe812e37105cdf28f66ea8b65ddbed1c119
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2022-02-13 17:08:42 +0300

    day 17

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ilgın Akgöz on 4.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
