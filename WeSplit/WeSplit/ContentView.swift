97a31055dc442b02ea3ce316daa90dd3f7736de4
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2022-03-07 18:42:37 +0300

    total amount text view turns red if user selects a 0% tip

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -72,6 +72,7 @@ struct ContentView: View {
                 
                 Section {
                     Text(totalAmount, format: currencyFormatter)
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
                 } header: {
                     Text("Total Amount")
                 }
