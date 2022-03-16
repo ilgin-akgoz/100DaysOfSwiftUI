3b0b745e7e43eb16360ef84ec4900a597972f4b9
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2022-03-16 15:43:02 +0300

    sleep calculator based on coreML

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ilgın Akgöz on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
